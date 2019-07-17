731047133c709dcb0b3f7added2115e958bf4b7c
kaushikmajumder <kaushikmajumder@192.168.1.4>
2019-07-17 10:34:34 +0530

    Update to Update branch by chnaging Class A

--- ClassA.java
+++ ClassA.java
@@ -4,5 +4,7 @@ public class ClassA {
 	
 	This Class has chnages from the Master branch 
 	The Master branch will create 2 more branches - Branch Project, Branch Update
+	
+	*** This is the Update Branch ** which will be pulled by the project branch for updates
 
 }
